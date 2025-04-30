f000f9cda6618a8706a9fed2020c1a317aa76686
Alexander199824 <mismaleje@miumg.edu.gt>
2025-04-30 11:32:05 -0600

    version 1-4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {
-        animation: {
-          bounce: 'bounce 1s infinite',
-          pulse: 'pulse 1.5s infinite',
-          ping: 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
-        },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        bounce: 'bounce 1s infinite',
+        pulse: 'pulse 1.5s infinite',
+        ping: 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
